af05d88735b2c347a09f6cacb586e54ce1d270b0
super1shy <supershynj3@gmail.com>
2024-01-08 23:23:13 +0300

    fixes

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 // routes import
+const authRouter = require('./routes/authRouter');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // routes
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server has started on PORT ${PORT}`);
