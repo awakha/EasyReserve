5072b83e9a0f666c23c36f402a26c0889f496f23
awakha <pagalova97@mail.ru>
2024-01-12 16:01:51 +0300

    login/logout

--- client/src/components/pages/authPage/Register.tsx
+++ client/src/components/pages/authPage/Register.tsx
@@ -1,9 +1,40 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { CustomLayout } from "../../Layout/CustomLayout";
+import authAxiosInstance from "../../../http";
+import AuthService from "../../../services/AuthService";
+import { useDispatch } from "react-redux";
+import { loginAsync } from "../../../store/slices/authSlice";
 
 const Register = () => {
-  return <CustomLayout></CustomLayout>;
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      await dispatch(loginAsync(email, password));
+    } catch (error) {
+      console.error("Ошибка входа:", error);
+    }
+  };
+  return (
+    <CustomLayout>
+      <input
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+    </CustomLayout>
+  );
 };
 
 export default Register;
