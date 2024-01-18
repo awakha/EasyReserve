4ba818328c25b588f339e8b192f5dcafee2efef1
Ursovdeni <dursov1991@gmail.com>
2024-01-18 13:28:24 +0300

    addReserveAdmin

--- backend/src/routes/profileRouter.js
+++ backend/src/routes/profileRouter.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const profileController = require('../controllers/profileController');
 
 router.get('/', profileController.getReservations);
+router.get('/admin', profileController.getReservationsByAdmin);
 router.post('/', profileController.postReview);
 router.delete('/:reservationId', profileController.deleteReservation);
 
